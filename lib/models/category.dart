e66775cf420abda7571327030f9549d517a00ee6
Nipuna Madula <nipunamadula2001@gmail.com>
2025-06-22 23:00:25 +0530

    Add Category model with predefined categories

---
+++ lib/models/category.dart
@@ -0,0 +1,20 @@
+class Category {
+  final String name;
+  final String icon;
+
+  Category({
+    required this.name,
+    required this.icon,
+  });
+
+  final List<Category> predefinedCategories = [
+    Category(name: 'Food', icon: '🍔'),
+    Category(name: 'Transport', icon: '🚌'),
+    Category(name: 'Health', icon: '💊'),
+    Category(name: 'Utilities', icon: '💡'),
+    Category(name: 'Entertainment', icon: '🎬'),
+    Category(name: 'Shopping', icon: '🛍️'),
+    Category(name: 'Others', icon: '📦'),
+  ];
+  
+}
