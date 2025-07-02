7679cbe9622c1654ff8d65c0766c21352613a547
Nipuna Madula <nipunamadula2001@gmail.com>
2025-07-02 12:29:30 +0530

    Register BudgetProvider using MultiProvider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/expense_provider.dart';
+import 'providers/budget_provider.dart';
 import 'screens/home_screen.dart';
 
 void main() {
-  runApp(SaveMateApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ExpenseProvider()),
+        ChangeNotifierProvider(create: (_) => BudgetProvider()),
+      ],
+      child: SaveMateApp(),
+    ),
+  );
 }
 
 class SaveMateApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => ExpenseProvider()),
-      ],
-      child: MaterialApp(
-        title: 'SaveMate',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.green,
-        ),
-        home: HomeScreen(),
+    return MaterialApp(
+      title: 'SaveMate',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
       ),
+      home: HomeScreen(),
     );
   }
 }
